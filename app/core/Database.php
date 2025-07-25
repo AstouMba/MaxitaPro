a087d2df4233f24cee5448b44eceb858a8b05f8f
AstouMba <assalih1303@gmail.com>
2025-07-25 19:46:10 +0000

    0010

--- app/core/Database.php
+++ app/core/Database.php
@@ -10,11 +10,22 @@ class Database
     private  PDO $pdo;
     private static ?Database $instance = null;
     public function __construct(){
-        
+//         DB_USERNAME=postgres
+// DB_PASSWORD=BBpzoFKMWtZOlCOnvmtuNFJPnhTfISFu
+// DB_HOST=switchyard.proxy.rlwy.net
+// DB_NAME=railway
+// DB_PORT=42194
+// DB_DRIVE=pgsql
+
+$dns="pgsql:host=switchyard.proxy.rlwy.net;port=42194;dbname=railway";
+$dbname="railway";
+$password="BBpzoFKMWtZOlCOnvmtuNFJPnhTfISFu";
+
+
         try{
         // var_dump(DSN, DB_USERNAME, DB_PASSWORD); die;
 
-            $this->pdo = new PDO(DSN, DB_USERNAME, DB_PASSWORD);
+            $this->pdo = new PDO($dns,  $dbname,$password);
         }catch(PDOException $e){
             echo "Error: " . $e->getMessage();
         }
