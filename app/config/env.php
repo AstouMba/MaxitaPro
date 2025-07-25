3f7e82b4a34274fa7d41e5be549af53451744223
AstouMba <assalih1303@gmail.com>
2025-07-25 17:37:59 +0000

    009

--- app/config/env.php
+++ app/config/env.php
@@ -6,7 +6,7 @@ $dotenv = Dotenv::createImmutable(__DIR__  .'/../../');
 $dotenv->load();
 
 // define('APP_URL', $_ENV['APP_URL']);
-define("DB_USERNAME", $_ENV["DB_USERNAME"]); 
+define("DB_USER", $_ENV["DB_USER"]); 
 define("DB_PASSWORD", $_ENV["DB_PASSWORD"]);
 define("DSN", $_ENV["DSN"]);
 // define("TWILIO_SID", $_ENV["TWILIO_SID"]);
